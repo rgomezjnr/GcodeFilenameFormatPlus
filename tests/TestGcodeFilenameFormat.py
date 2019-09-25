3e11ba2aa37b46d1a0555ffdc9171ecd262cd6d2
rgomezjnr <rgomezjnr@gmail.com>
2019-09-24 23:04:40 -0700

    Added unit test stub code

---
+++ tests/TestGcodeFilenameFormat.py
@@ -0,0 +1,15 @@
+import unittest
+from GcodeFilenameFormat import GcodeFilenameFormat
+
+class TestGcodeFilenameFormat(unittest.TestCase):
+
+    def test_getModifiedPrintSettings():
+
+    def test_getDefaultPrintSettings():
+
+    def test_logPrintSettings():
+
+    def test_addPrintSettingsToFilename():
+
+if __name__ == '__main__':
+    unittest.main()
