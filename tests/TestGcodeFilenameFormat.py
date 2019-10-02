1dacc7cd7c9a9ec337c5a034c7b911c894e71fca
rgomezjnr <rgomezjnr@gmail.com>
2019-10-02 05:34:57 -0700

    Added additional tests to test_getPrintSettings()

--- tests/TestGcodeFilenameFormat.py
+++ tests/TestGcodeFilenameFormat.py
@@ -1,15 +1,38 @@
 import unittest
 from GcodeFilenameFormat import GcodeFilenameFormat
+#import GcodeFilenameFormat
+#from ../ import GcodeFilenameFormat
+#target = __import__("../GcodeFilenameFormat.py")
 
 class TestGcodeFilenameFormat(unittest.TestCase):
 
-    def test_getModifiedPrintSettings():
+    def test_getPrintSettings(self):
+        # Normal profile print settings, Generic PLA
+        input = {'layer_height': 0.15,
+                'infill_spare_density': 10,
+                'default_material_print_temperature': 200,
+                'material_bed_temperature': 60}
 
-    def test_getDefaultPrintSettings():
+        #print_settings = getPrintSettings()
+        print_settings = target.getPrintSettings()
 
-    def test_logPrintSettings():
+        # assert that machine manager called to obtain global stack for retrieving print settings
+        # assert that print settings match test input
+        self.assertDictEqual(input, print_settings)
+        # assert that print_settings type is dict
+        self.assertIsInstance(print_settings, dict)
+        # assert that print settings is not empty
+        self.assertIsNotNone(print_settings)
 
-    def test_addPrintSettingsToFilename():
+    #def test_getModifiedPrintSettings():
+
+    #def test_getDefaultPrintSettings():
+
+    #def test_logPrintSettings():
+        # assert that Logger.log called
+
+    #def test_addPrintSettingsToFilename():
+        # assert that return object is string
 
 if __name__ == '__main__':
     unittest.main()
