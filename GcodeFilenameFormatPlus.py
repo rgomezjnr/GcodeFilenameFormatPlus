24881aa2548c316bdbc6377416df16d1737dd464
DD_DE <78473530+DDdotDE@users.noreply.github.com>
2023-10-18 12:40:41 +0200

    readded a format

--- GcodeFilenameFormatPlus.py
+++ GcodeFilenameFormatPlus.py
@@ -167,7 +167,8 @@ class GcodeFilenameFormatPlus(Extension, QObject):
             return None
 
         time = QDateTime.currentDateTime().toString("HH-mm")
-        year =  QDateTime.currentDateTime().toString("yy")
+        year =  QDateTime.currentDateTime().toString("yyyy")   
+        year2 =  QDateTime.currentDateTime().toString("yy")
         month = QDateTime.currentDateTime().toString("MM")
         day = QDateTime.currentDateTime().toString("dd")
         hour = QDateTime.currentDateTime().toString("HH")
@@ -175,7 +176,7 @@ class GcodeFilenameFormatPlus(Extension, QObject):
         second = QDateTime.currentDateTime().toString("ss")
         date = year + '-' + month + '-' + day
         datetime = date + 'T' + hour + minute + second
-        counter_down = str(1000000 - (int(year)*10000) - (int(month)*1000) - (int(day)*100) - (int(hour)*10) - int(minute))
+        counter_down = str(1000000 - (int(year2)*10000) - (int(month)*1000) - (int(day)*100) - (int(hour)*10) - int(minute))
 
         tokens = re.split(r'\W+', filename_format)      # TODO: split on brackets only
 
